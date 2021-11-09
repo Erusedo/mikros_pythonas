074df70b13daeb3fdf5e563eec8597463934faa8
Erusedo <erusedo@gmail.com>
2021-11-10 00:44:49 +0200

    idk

---
+++ modules.py
@@ -0,0 +1,6 @@
+#https://docs.python.org/3/py-modindex.html or search for modules that I need
+
+import tools
+
+print(tools.roll_dice(10))
+
--- read_files/reading_files.py
+++ read_files/reading_files.py
@@ -1,7 +1,7 @@
 # w - write     r - read    a - append (can't modify info but can add)  r+ - read&write
 
 employee_file = open("/Volumes/Terabyte/Programming/mikros_pythonas/read_files/employees.txt", "r")
-
-print(employee_file.read())
+for employee in employee_file.readlines():
+    print(employee)
 
 employee_file.close()
