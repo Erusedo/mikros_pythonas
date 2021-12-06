a9828197c3bbdfafc63740b6f5b2766a9a5966e1
Erusedo <erusedo@gmail.com>
2021-12-06 17:53:08 +0200

    idk

--- modules.py
+++ modules.py
@@ -1,4 +1,4 @@
-#https://docs.python.org/3/py-modindex.html or search for modules that I need
+#https://docs.python.org/3/py-modindex.html or search for modules that I need on Google
 
 import tools
 
